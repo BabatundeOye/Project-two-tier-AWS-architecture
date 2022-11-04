0bc824d9aa3903ef2d6c75a3e4f1b7cf7d56c7ff
BabatundeOye <teamsoldd@gmail.com>
2022-11-04 22:25:28 +0000

    weekly_project_two_tier

--- main.tf
+++ main.tf
@@ -1,21 +1,12 @@
 #---root/main.tf
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.38.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
 
 module "networking" {
-  source   = "./networking"
-  vpc_cidr = "10.0.0.0/16"
-  public_cidrs = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
-  private_cidrs = ["10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24"]
+  source           = "./networking"
+  vpc_cidr         = "10.0.0.0/16"
+  public_sn_count  = 3
+  private_sn_count = 3
+  max_subnets = 10
+  public_cidrs     = [for i in range(1, 255, 2) : cidrsubnet("10.0.0.0/16", 8, i)]
+  private_cidrs    = [for i in range(2, 255, 2) : cidrsubnet("10.0.0.0/16", 8, i)]
 
 }
--- networking/variables.tf
+++ networking/variables.tf
@@ -4,9 +4,21 @@ variable "vpc_cidr" {
 }
 
 variable "public_cidrs" {
-  type = list
+  type = list(any)
 }
 
 variable "private_cidrs" {
-  type = list
+  type = list(any)
+}
+
+variable "public_sn_count" {
+  type = number
+}
+
+variable "private_sn_count" {
+  type = number
+}
+
+variable "max_subnets" {
+  type = number
 }

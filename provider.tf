0100d60544d06a673f9559fb0f869e844cc677aa
BabatundeOye <teamsoldd@gmail.com>
2022-11-05 04:37:04 +0000

    weekly_project_two_tier

---
+++ provider.tf
@@ -0,0 +1,13 @@
+#---root/provider.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.38.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
